8a8a78c3413600206dba2c873d0d09a5bcb74fda
kszuba <kszuba@networkedassets.org>
2025-12-06 16:46:47 +0100

    feat: allow server to check who-is-online to

--- src/chat_server.erl
+++ src/chat_server.erl
@@ -1,8 +1,8 @@
 -module(chat_server).
 -author("kszuba").
--export([start/0, loop/1]).
+-export([start/0, print_users/0, loop/1]). %% Added print_users
 
-%% @doc Starts the chat server process and registers it globally.
+%% @doc Starts the chat server process.
 start() ->
   io:format("Chat server starting...~n"),
   Pid = spawn(?MODULE, loop, [[]]),
@@ -10,7 +10,13 @@ start() ->
   io:format("Chat server started with Pid ~p~n", [Pid]),
   Pid.
 
-%% @doc The main server loop. Clients is a list of {Nick, Pid}.
+%% @doc NEW: API function to trigger printing users on the server console
+print_users() ->
+  %% Send a specific atom to the registered server process
+  chat_server ! show_users_console,
+  ok.
+
+%% @doc The main server loop.
 loop(Clients) ->
   receive
     {subscribe, ListenerPid, Nick} ->
@@ -36,16 +42,26 @@ loop(Clients) ->
           loop(Clients)
       end;
 
-  %% NEW: Handle "Who is online" request
     {get_users, RequesterPid} ->
       io:format("Server: Sending user list to ~p~n", [RequesterPid]),
-      %% Extract only the Nicknames from the state tuples
-      %% Clients = [{Nick, Pid}, ...] -> Nicks = [Nick, ...]
       Nicks = [N || {N, _} <- Clients],
       RequesterPid ! {users_list, Nicks},
+      loop(Clients);
+
+  %% NEW: Handle the admin request to print users locally
+    show_users_console ->
+      io:format("~n--- Admin: Connected Users ---~n"),
+      print_clients_internal(Clients),
+      io:format("------------------------------~n"),
       loop(Clients);
 
     Other ->
       io:format("Server: Received unknown message: ~p~n", [Other]),
       loop(Clients)
   end.
+
+%% Helper to print the list of tuples
+print_clients_internal([]) -> ok;
+print_clients_internal([{Nick, Pid} | T]) ->
+  io:format(" User: ~-10s | Pid: ~p~n", [Nick, Pid]),
+  print_clients_internal(T).
